632e542dbab90b7571e620f8c0894617ee313cf7
conankzhang <conankzhang@gmail.com>
2018-06-23 20:49:04 -0600

    mult: implement

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,50 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+(INIT)
+    @i
+    M = 1 // i = 1
+
+    @R0
+    D = M
+    @n
+    M = D // n = R0
+
+    @R1
+    D = M
+    @factor
+    M = D // factor = R1
+
+    @product
+    M = 0 // product = 0
+
+(LOOP)
+    @i
+    D = M
+    @n
+    D = D-M
+    @STOP
+    D;JGT // if i > n goto STOP
+
+    @product
+    D=M
+    @factor
+    D=D+M
+    @product
+    M=D //product = product + factor
+    @i
+    M=M+1 // i = i + 1
+
+    @LOOP
+    0;JMP
+
+(STOP)
+    @product
+    D=M
+    @R2
+    M=D // RAM[2] = product
+
+(END)
+    @END
+    0;JMP // infinite loop to end
