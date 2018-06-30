c6403b1a8159225e8573849937d7fc9ebdb56301
conankzhang <conankzhang@gmail.com>
2018-06-30 04:40:26 -0600

    chore(parser): set up parse instructions

--- 06/assembler/parser.py
+++ 06/assembler/parser.py
@@ -1,12 +1,12 @@
 class Parser:
     def parse(self, line):
         if line[0] == '@':
-            __parse_a_instruction(line)
+            self._parse_a_instruction(line)
         else:
-            __parse_c_instruction(line)
+            self._parse_c_instruction(line)
 
-    def __parse_a_instruction(self, line):
+    def _parse_a_instruction(self, line):
         print('yay')
 
-    def __parse_c_instruction(self, line):
+    def _parse_c_instruction(self, line):
         print('k')
