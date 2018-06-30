3482a595b7bcc2e4fbfb9ef22f1e8f5bf2f8c409
conankzhang <conankzhang@gmail.com>
2018-06-30 06:21:19 -0600

    chore(converter): built lookup tables

--- 06/assembler/converter.py
+++ 06/assembler/converter.py
@@ -1,18 +1,54 @@
 class Converter:
     def __init__(self):
         self.comp_table = {
+            '0' : "0101010",
+            '1' : "0111111",
+            '-1' : "0111010",
+            'D' : "0001100",
             'A' : "0110000",
+            '!D' : "0001101",
+            '!A' : "0110001",
+            '-D' : "0001111",
+            '-A' : "0110011",
+            'D+1' : "0011111",
+            'A+1' : "0110111",
+            'D-1' : "0001110",
+            'A-1' : "0110010",
             'D+A' : "0000010",
-            'D' : "0001100"
+            'D-A' : "0010011",
+            'A-D' : "0000111",
+            'D&A' : "0000000",
+            'D|A' : "0010101",
+            'M' : "1110000",
+            '!M' : "1110001",
+            '-M' : "1110011",
+            'M+1' : "1110111",
+            'M-1' : "1110010",
+            'D+M' : "1000010",
+            'D-M' : "1010011",
+            'M-D' : "1000111",
+            'D&M' : "1000000",
+            'D|M' : "1010101"
         }
 
         self.dest_table = {
+            'M' : "001",
             'D' : "010",
-            'M' : "001"
+            'MD' : "011",
+            'A' : "100",
+            'AM' : "101",
+            'AD' : "110",
+            'AMD' : "111"
         }
 
         self.jump_table = {
             'JGT' : "001",
+            'JEQ' : "010",
+            'JGE' : "011",
+            'JLT' : "100",
+            'JNE' : "101",
+            'JLE' : "110",
+            'JMP' : "111"
         }
 
     def convert_a_instruction(self, value):
@@ -21,9 +57,11 @@ class Converter:
     def convert_c_instruction(self, dest, comp, jump):
         begin = '111'
         comp_binary = self.comp_table[comp]
-        dest_binary = self.dest_table[dest]
+        dest_binary = "000"
         jump_binary = "000"
 
+        if dest is not None:
+            dest_binary = self.dest_table[dest]
         if jump is not None:
             jump = self.jump_table[jump]
 
