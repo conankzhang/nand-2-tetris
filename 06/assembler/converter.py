db23caaec040053ea370245c41ff7d7a7985cf5f
conankzhang <conankzhang@gmail.com>
2018-06-30 05:50:36 -0600

    feat(parser): parse dest, comp and jump from instruction

--- 06/assembler/converter.py
+++ 06/assembler/converter.py
@@ -1,3 +1,6 @@
 class Converter:
     def convert_a_instruction(self, value):
         return format(value, '016b')
+
+    def convert_c_instruction(self, dest, comp, jump):
+        return "okay"
--- 06/assembler/parser.py
+++ 06/assembler/parser.py
@@ -15,4 +15,13 @@ class Parser:
         return self.converter.convert_a_instruction(value)
 
     def _parse_c_instruction(self, line):
-        print('k')
+        begin = '111'
+        dest = line[:line.index("=")]
+        comp = line[line.index("=")+1:]
+        jump = None
+
+        if line.find(";") is not -1:
+            comp = line[line.index("=")+1:line.index(";")]
+            jump = line[line.index(";")+1:]
+
+        return self.converter.convert_c_instruction(dest, comp, jump)
